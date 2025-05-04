ad7e2d5850675c794371752c55075ec33bbb8726
DOdbg <jteetouch2552@gmail.com>
2025-05-04 12:25:08 +0700

    add A2-10

--- A2/10.cpp
+++ A2/10.cpp
@@ -20,8 +20,8 @@ template<typename T> ostream &operator << (ostream &out, vector<T> v){
 const int inf = 1e9 + 7;
 
 struct dep {
-    int d, l, r;
-    dep(int nd, int nl, int nr) : d(nd), l(nl), r(nr) {}
+    int d, l;
+    dep(int nd, int nl) : d(nd), l(nl) {}
 };
 
 int main()
@@ -29,29 +29,56 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-    int n;
-    cin >> n;
-    vector<dep> v(n);
-    int depth = 0, dist = 0;
-    for (auto &[d, l, r]:v){
-        int delta_depht, delta_dist;
-        cin >> delta_depht >> delta_dist;
-        depth += delta_depht;
-        d = depth;
-        l = dist;
-        dist += delta_dist;
-        r = dist;
-    }
-    v.push_back(dep(0, dist, inf));
+    int n, q;
+    cin >> n >> q;
     stack<dep> st;
-    st.push(dep(0, 0, 0));
-    map<int, int> bruh;
-    int nl = 0, nr = 0;
-    for (auto [d, l, r]:v){
-        while (!st.empty() && d <= st.top().d){
-            
+    vector<int> depht(n + 1, 0);
+    int dep_n = 0, curr = 0, mx_dep = 0;
+    for (int i = 0;i < n;i++){
+        int d, l;
+        cin >> d >> l;
+        dep_n += d;
+        mx_dep = max(mx_dep, dep_n);
+
+        while (!st.empty() && st.top().d > dep_n){
+            depht[st.top().d] = max(depht[st.top().d], curr - st.top().l);
+            st.pop();
         }
-        st.push(dep(d, l, r));
+        st.push({dep_n, curr});
+        curr += l;
+    }
+    while (!st.empty() && st.top().d == dep_n){
+        depht[st.top().d] = max(depht[st.top().d], curr - st.top().l);
+        st.pop();
     }
 
+    depht[0] = 1e9;
+
+    while (q--){
+        int x;
+        cin >> x;
+        int lb = 0, ub = mx_dep, ans = 0;
+        while (lb <= ub){
+            int mid = lb + (ub - lb) / 2;
+            if (depht[mid] >= x){
+                ans = mid;
+                lb = mid + 1;
+            }
+            else
+                ub = mid - 1;
+        }
+        cout << ans << '\n';
+    }
 }
+/*
+9 5
+1 2
+1 3
+1 1
+1 3
+-1 1
+-1 1
+1 5
+-1 1
+-1 2
+*/
